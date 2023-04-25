6113637cd15241816fd8c00f3e2c5293200a4e00
admin <admin@BCS-Ubuntu-37.lrccd.ad.losrios.edu>
2023-04-25 15:50:41 -0700

    ubuntu

--- Mandelbrot/main.cpp
+++ Mandelbrot/main.cpp
@@ -1,8 +1,13 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+using namespace sf;
+using namespace std;
 
 int main()
 {
+	float width = VideoMode::getDesktopMode().width;
+	float height = VideoMode::getDesktopMode().height;
+	float aspectRatio = height / width;
 
 
 }
